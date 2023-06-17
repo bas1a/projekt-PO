a30feaaeff08d3f8bc51bc78394e838e3d5f4c6d
bas1a <136897752+bas1a@users.noreply.github.com>
2023-06-17 13:49:58 +0200

    Update Table.cs

---
+++ Table.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Table
+{
+    public int[] TableRandom()
+    {
+        int[] table = new int[500];
+        Random rnd = new Random();
+        for (int i = 0; i < table.Length; i++)
+        {
+            table[i] = rnd.Next(1, 50001);
+        }
+        return table;
+    }
+
+
+
+    public int[] TableIncrease()
+    {
+        int[] table = new int[500];
+        for (int i = 0; i < table.Length; i++)
+        {
+            table[i] = i;
+        }
+        return table;
+    }
+
+
+
+    public int[] TableDecrease()
+    {
+        int n = 500;
+        int[] table = new int[n];
+        for (int i = 0; i < table.Length; i++)
+        {
+            table[i] = n-1-i;
+        }
+        return table;
+    }
+}
